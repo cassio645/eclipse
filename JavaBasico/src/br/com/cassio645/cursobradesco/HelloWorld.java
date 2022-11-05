8afd764aa9be70bd29c27851dd52722995714e50
cassio645 <cantonio645@gmail.com>
2022-11-05 15:16:14 -0300

    Finalizando as aulas Básicas do Bradesco

--- JavaBasico/src/br/com/cassio645/cursobradesco/HelloWorld.java
+++ JavaBasico/src/br/com/cassio645/cursobradesco/HelloWorld.java
@@ -1,18 +1,16 @@
-/**
- * 
- */
 package br.com.cassio645.cursobradesco;
 
 /**
+ * Programa que imprime Hello World! e na linha debaixo 2022
  * @author cassio
- * Testando Hello World
  */
 public class HelloWorld {
 
+	/* Método principal(main) da classe HelloWorld */
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("2022");
 
-	}
+	} // Fim do método
 
-}
+} // Fim da classe
