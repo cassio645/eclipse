270a36ee7c13441f5f7b2261247d30e0e8ec526c
cassio645 <51422880+cassio645@users.noreply.github.com>
2022-11-05 09:59:58 -0300

    Adicionei os primeiros arquivos

---
+++ JavaBasico/src/br/com/cassio645/cursobradesco/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package br.com.cassio645.cursobradesco;
+
+/**
+ * @author cassio
+ * Testando Hello World
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
---
+++ JavaBasico/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author cassio
+ *
+ */
+module JavaBasico {
+}
